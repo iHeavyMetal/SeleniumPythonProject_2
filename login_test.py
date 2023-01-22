67cb4eccac1914151c92a11f39bea78cd9b9bfc8
HeavyMetal <dziobal294@gmail.com>
2023-01-22 11:09:27 +0100

    Create acc. Login failed/passed.

---
+++ login_test.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def test_log_in_passed():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    driver.get("http://seleniumdemo.com/")
+    driver.find_element(By.XPATH, "//li[@id='menu-item-22']//a").click()
+    driver.find_element(By.ID, "username").send_keys("HeavyMetal@gmail.com")
+    driver.find_element(By.ID, "password").send_keys("Frytki123!")
+    driver.find_element(By.ID, "password").send_keys(Keys.ENTER)   #login by click ENTER_key
+    #driver.find_element(By.XPATH, "//button[@class='woocommerce-Button button']").click()
+    #driver.find_element(By.NAME, "login").click()
+
+    assert driver.find_element(By.LINK_TEXT, "Logout").is_displayed()
+    time.sleep(5)
+
+# def test_log_in_failed():
+#     driver = webdriver.Chrome(ChromeDriverManager().install())
+#     driver.maximize_window()
+#     driver.get("http://seleniumdemo.com/")
+#     driver.find_element(By.XPATH, "//li[@id='menu-item-22']//a").click()
+#     driver.find_element(By.ID, "username").send_keys("HeavyMetal@gmail.com")
+#     driver.find_element(By.ID, "password").send_keys("Frytki123")
+#     driver.find_element(By.ID, "password").send_keys(Keys.ENTER)
+#     assert "ERROR: Incorrect username or password." in driver.find_element(By.XPATH, "//ul[@class='woocommerce-error']//li").text
+#     time.sleep(5)
